affb7b683087f451cef7094da40180e10c54ce9d
Saray Soldado-Magraner <saraysoldado@gmail.com>
2021-09-08 17:45:09 -0700

    initial commit

---
+++ Fig5_crosshomeo_demo_multi_unit.m
@@ -0,0 +1,559 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Multi-unit rate-based population model with homeostatic Up development rules. 
+% ssaray@ucla.edu
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% SIMULATION SETTINGS %%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all
+close all
+
+dt = 0.0001; %IN SECONDS
+tmax   = 2/dt; %
+nTrial = 200; %100
+
+rng(44)
+    
+GRAPHICS = 1;
+VIDEO =0;
+HOMEOSTATIC_FLAG = 1;
+
+
+learning_rule= 'cross-homeo'; 
+
+savetrials = [1,2,5,40,200]; 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% NEURON PARAMETERS %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+F = @(x,gain,Thr) gain*max(0,x-Thr);
+
+Ne = 80;
+Ni = 20;
+
+thetaE = 4.8;
+thetaI = 25;
+gainE = 1;
+gainI = 4;
+
+Etau = 10/(dt*1000); %10
+Itau = 2/(dt*1000);  %2
+
+Beta = 0;
+tauA = 500;
+
+E_MAX = 100;
+I_MAX = 250;
+
+OUtau = 1/10; %Ornstein-Uhlenbeck Noise
+OUmu = 0;
+OUsigma = 0.1; 
+
+OUE = zeros(Ne,1);
+OUI = zeros(Ni,1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% INIT WEIGHT MATRIX %%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% COMPLETELY RANDOM
+
+WEE = rand(Ne,Ne)*0.16; 
+WEE = WEE - diag(diag(WEE));
+
+WEI = rand(Ne,Ni)*0.16;
+
+WIE= rand(Ni,Ne)*0.16;
+
+WII = rand(Ni,Ni)*0.16;
+WII = WII - diag(diag(WII));
+
+%%
+Winit = [WEE,WEI;WIE,WII];
+
+WEEp = sum(WEE,2);
+WEIp = sum(WEI,2);
+WIEp = sum(WIE,2);
+WIIp = sum(WII,2);
+
+initWEEp = WEEp;
+initWEIp = WEIp;
+initWIEp = WIEp;
+initWIIp = WIIp;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PLASTICITY %%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ExSet = 5; %setpoint for excitatory neurons
+InhSet = 14; 
+
+tau_trial = 2 ;
+
+WEI_MIN = 0.1;
+WEE_MIN = 0.1;
+WII_MIN = 0.1;
+WIE_MIN = 0.1;
+
+alpha = 0.00002; %learning rate 0.00001 / 0.00002
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% INIT VARS %%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+trialhistFCa      = NaN(nTrial,Ne);
+trialhistFCaInh   = NaN(nTrial,Ni);
+trialhistWEE      = NaN(nTrial,Ne);
+trialhistWEI      = NaN(nTrial,Ne);
+trialhistWIE      = NaN(nTrial,Ni);
+trialhistWII      = NaN(nTrial,Ni);  
+trialhistWEEp      = NaN(nTrial,Ne);
+trialhistWEIp      = NaN(nTrial,Ne);
+trialhistWIEp      = NaN(nTrial,Ni);
+trialhistWIIp      = NaN(nTrial,Ni);  
+
+ExAvg = zeros(Ne,1);
+InhAvg = zeros(Ni,1);
+
+hR = zeros(tmax,Ne+Ni); %history of Inh and Ex rate 
+
+
+EvokedOn = 0.250/dt; %Evoked current
+EvokedDur = 0.01/dt; %0.01
+EvokedAmp = 7; %7
+
+counter = 0;  
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PLOTTING %%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if GRAPHICS
+    multi_unit_UpDev_graphics_crosshomeo
+end
+
+if VIDEO
+      counter = counter+1;
+      frames(counter) = getframe(h1); 
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% SIMULATION %%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for trial=1:nTrial    
+      
+      fCa = zeros(Ne,tmax);  %instantaneous fast Ca sensor, integrates the firing rate of E
+      fCaInh = zeros(Ni,tmax);  %instantaneous fast Ca sensor, integrates the firing rate of I
+      
+      hR = zeros(tmax,Ne+Ni); %history of Inh and Ex rate 
+      
+     E = zeros(Ne,1);
+     I = zeros(Ni,1);
+     a = zeros(Ne,1);
+      
+           
+      evoked = zeros(1,tmax);
+      evoked(EvokedOn:EvokedOn+EvokedDur)=EvokedAmp;
+      
+%%
+      for t=1:tmax        
+         
+         OUE = OUE + OUtau*(OUmu-OUE) + OUsigma*randn(Ne,1); %Ornstein-Uhlenbeck Noise for excitatory unit
+         OUI = OUI + OUtau*(OUmu-OUI) + OUsigma*randn(Ni,1); %Ornstein-Uhlenbeck Noise for inhibitory unit
+         
+         E = E + (-E + F(WEE*E - WEI*I - a + evoked(t) + OUE,gainE,thetaE) )/Etau;
+         I = I + (-I + F(WIE*E - WII*I + OUI,gainI,thetaI) )/Itau;
+         
+         a = a + (-a + Beta*E)/tauA; %ADAPTATION 
+         
+         Emaxvec = E>E_MAX; E(Emaxvec) = E_MAX; %Neurons have a saturation of their rates
+         Imaxvec = I>I_MAX; I(Imaxvec) = I_MAX;
+
+         
+         hR(t,:) = [E' I'];
+         
+         % Ex Ca Sensors
+         fCa(:,t) = E;
+         fCaInh(:,t) = I;
+         
+         
+      end
+      
+         ExAvg    = ExAvg  + (-ExAvg  + mean(fCa(:,end-0.5/dt:end),2))/tau_trial; %we average at the end of the trial to avoid evoked
+         InhAvg   = InhAvg + (-InhAvg + mean(fCaInh(:,end-0.5/dt:end),2))/tau_trial;
+         
+      
+    %%  
+    
+      WEEp = sum(WEE,2); %update sum of presynaptic weights for plot
+      WEIp = sum(WEI,2);
+      WIEp = sum(WIE,2);
+      WIIp = sum(WII,2);
+     
+      trialhistFCa(trial,:) = ExAvg;
+      trialhistFCaInh(trial,:) = InhAvg;
+      trialhistWEE(trial,:) = WEE(:,end);      
+      trialhistWEI(trial,:) = WEI(:,end);     
+      trialhistWIE(trial,:) = WIE(:,end);   
+      trialhistWII(trial,:) = WII(:,end);            
+      trialhistWEEp(trial,:) = WEEp;
+      trialhistWEIp(trial,:) = WEIp;
+      trialhistWIEp(trial,:) = WIEp;
+      trialhistWIIp(trial,:) = WIIp;
+      
+      x=WEEp;
+      y1=WEIp;
+      [R,p] = corr(x,y1,'rows','complete');
+      P = polyfit(x,y1,1);
+      yfit = P(1)*x+P(2);ix=WIEp;
+      iy1=WIIp;
+      [iR,ip] = corr(ix,iy1,'rows','complete');
+      iP = polyfit(ix,iy1,1);
+      iyfit = iP(1)*ix+iP(2);
+      
+      
+      if HOMEOSTATIC_FLAG %&& trial>10
+          
+  
+
+            EAvg =  max(1,ExAvg); %For trials that start with 0 rate (development settings), otherwise weights would never move
+            IAvg = max(1,InhAvg);
+            
+            
+            newWEE = WEE + alpha*EAvg'*sum(InhSet-IAvg)/Ni;
+            newWEE = newWEE - diag(diag(newWEE));
+            
+            newWEI = WEI - alpha*IAvg'*sum(InhSet-IAvg)/Ni;
+
+            newWIE = WIE - alpha*EAvg'*sum(ExSet-EAvg)/Ne; 
+         
+            newWII = WII + alpha*IAvg'*sum(ExSet-EAvg)/Ne; 
+            newWII = newWII - diag(diag(newWII));
+                   
+         
+        WEE = newWEE; WEI = newWEI; WIE = newWIE; WII = newWII;
+         
+         %If weights fall below a minimum or are NaN set to minimum
+         
+         WEE(WEE<WEE_MIN/(Ne-1))=WEE_MIN/(Ne-1);
+         WEE(isnan(WEE))=WEE_MIN/(Ne-1);
+         WEI(WEI<WEI_MIN/Ni)=WEI_MIN/Ni;
+         WEI(isnan(WEI))=WEI_MIN/Ni;
+         WIE(WIE<WIE_MIN/Ne)=WIE_MIN/Ne;
+         WIE(isnan(WIE))=WIE_MIN/Ne;
+         WII(WII<WII_MIN/(Ni-1))=WII_MIN/(Ni-1);
+         WII(isnan(WII))=WII_MIN/(Ni-1);
+         WEE = WEE - diag(diag(WEE));
+         WII = WII - diag(diag(WII));
+            
+
+      end
+     %%
+     
+      
+      
+      
+      
+      if GRAPHICS %&& rem(trial,10)==0
+      refreshdata(h1) 
+      drawnow   
+      
+          if ismember(trial,savetrials)
+              saveas(gcf,['trial',num2str(trial)],'jpg')
+              saveas(gcf,['trial',num2str(trial)],'svg')
+
+          end
+      
+      end
+      
+      if VIDEO
+      counter = counter+1;
+      frames(counter) = getframe(h1); 
+      end
+      
+      
+ end
+   
+ Wend = [WEE,WEI;WIE,WII];  
+ save(['METApopmodel_',learning_rule,'Ex',num2str(ExSet),'Inh',num2str(InhSet),'.mat'])
+ 
+   if VIDEO
+    video = VideoWriter([learning_rule,'_MultiUnit_demo']);
+    video.FrameRate = 2;
+    open(video)
+    writeVideo(video,frames);
+    close(video)
+    clear video
+    clear frames
+   end
+   
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PLOTTING %%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if GRAPHICS   
+
+    figure('Position', [10 10 1300 500]);     
+     subplot(1,2,1)
+     
+
+     imagesc(Winit)
+     cMap = getPyPlot_cMap('coolwarm',128);
+     %cMap = getPyPlot_cMap('bone',128);
+     %cMap = getPyPlot_cMap('bone_r',128);
+     %cMap = getPyPlot_cMap('RdYlGn',128);
+     colormap(cMap);
+     colorbar
+     xlabel('pre')
+     ylabel('post')
+     title('pre training')    
+     subplot(1,2,2)
+    imagesc(Wend)
+    colorbar
+    xlabel('pre')
+    ylabel('post')
+    title('post training')
+    saveas(gcf,'Winitend','jpg')
+    saveas(gcf,'Winitend','svg')
+
+    figure('Position', [10 10 1300 500]);     
+     subplot(1,2,1)
+     
+%% with inhibitory weights negative
+
+Winit2 = Winit .* [ones(Ne+Ni,Ne),ones(Ne+Ni,Ni)*-1] ;
+Wend2 = Wend .* [ones(Ne+Ni,Ne),ones(Ne+Ni,Ni)*-1] ;
+
+
+%cMapwarm = getPyPlot_cMap('RdBu',128);
+%cMapwarm = getPyPlot_cMap('bwr_r',128);
+cMapwarm = getPyPlot_cMap('Reds_r',128);
+cMapprgn = getPyPlot_cMap('PRGn',128);
+
+idx=[128:-1:64];
+%map_green_red = [cMapwarm(1:64,:);cMapprgn(64:end,:)];
+map_green_red = [cMapwarm(1:2:128,:);cMapprgn(64:end,:)];
+
+%%
+figure('Position', [10 10 1300 500]);     
+     subplot(1,2,1)
+     imagesc(Winit2)
+     %cMap = getPyPlot_cMap('coolwarm',128);
+     %cMap = getPyPlot_cMap('bone',128);
+     %cMap = getPyPlot_cMap('bone_r',128);
+     %cMap = getPyPlot_cMap('RdYlGn',128);
+     %cMap = getPyPlot_cMap('PRGn',128);
+     %colormap(cMap);
+     colormap(map_green_red);
+     colorbar
+     caxis([-0.2 0.2])
+     xlabel('pre')
+     ylabel('post')
+     title('pre training')    
+     subplot(1,2,2)
+    imagesc(Wend2)
+    colorbar
+    caxis([-0.2 0.2])
+    xlabel('pre')
+    ylabel('post')
+    title('post training')
+    saveas(gcf,'Winitend','jpg')
+    saveas(gcf,'Winitend','svg')
+    %%
+    figure
+    subplot(2,2,1)
+    histogram(Winit(1:Ne,1:Ne))
+    hold on
+    histogram(WEE)
+    xlabel('WEE')
+    subplot(2,2,2)
+    histogram(Winit(1:Ne,Ne+1:end))
+    hold on
+    histogram(WEI)
+    legend('pre','post')
+    xlabel('WEI')
+    subplot(2,2,3)
+    histogram(Winit(Ne+1:end,1:Ne))
+    hold on
+    histogram(WIE)
+    xlabel('WIE')
+    subplot(2,2,4)
+    histogram(Winit(Ne+1:end,Ne+1:end))
+    hold on
+    histogram(WII)
+    xlabel('WII')
+    saveas(gcf,'Winitendhistogram','jpg')
+
+    %%
+    figure('Position', [10 10 1200 500]);     
+     %suptitle(['Total current per neuron ',titlet])
+     subplot(1,2,1)
+     plot(WEE*E,WEI*I,'o','color',[0 0.5 0])
+     xlabel('WEE*E')
+     ylabel('WEI*I')
+     set(gca,'FontSize',20)
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=WEE*E;
+    y1=WEI*I;
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+
+    subplot(1,2,2)
+     plot(WIE*E,WII*I,'o','color',[1 0 0])
+     xlabel('WIE*E')
+     ylabel('WII*I')
+     set(gca,'FontSize',20)
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=WIE*E;
+    y1=WII*I;
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+    saveas(gcf,'currents','jpg')
+    
+    %%
+    figure('Position', [10 10 1200 500]);     
+     %suptitle(['Total current per neuron ',titlet])
+     subplot(1,2,1)
+     scatter(WEEp,WEIp,[60],'filled','MarkerFaceColor',[0 0.5 0],'MarkerFaceAlpha',0.6)
+     %plot(WEEp,WEIp,'o','color',[0 0.5 0])
+     xlabel('WEE')
+     ylabel('WEI')
+     set(gca,'FontSize',20)
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=WEEp;
+    y1=WEIp;
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2,'color',[0, 0.4470, 0.7410]);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+
+    subplot(1,2,2)
+    scatter(WIEp,WIIp,[60],'filled','MarkerFaceColor',[1 0 0],'MarkerFaceAlpha',0.6)
+     %plot(WIEp,WIIp,'o','color',[1 0 0])
+     xlabel('WIE')
+     ylabel('WII')
+     set(gca,'FontSize',20)
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=WIEp;
+    y1=WIIp;
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2,'color',[0, 0.4470, 0.7410]);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+    saveas(gcf,'preW','jpg')
+    saveas(gcf,'preW','svg')
+
+    %%
+     %%
+    figure('Position', [10 10 1200 500]);     
+     %suptitle(['Total current per neuron ',titlet])
+     subplot(1,2,1)
+     scatter(initWEEp,initWEIp,[60],'filled','MarkerFaceColor',[0 0.5 0],'MarkerFaceAlpha',0.6)
+     %plot(initWEEp,initWEIp,'o','color',[0 0.5 0])
+     xlabel('WEE')
+     ylabel('WEI')
+     set(gca,'FontSize',20)
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=initWEEp;
+    y1=initWEIp;
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2,'color',[0, 0.4470, 0.7410]);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+
+    subplot(1,2,2)
+     scatter(initWIEp,initWIIp,[60],'filled','MarkerFaceColor',[1 0 0],'MarkerFaceAlpha',0.6)
+     %plot(initWIEp,initWIIp,'o','color',[1 0 0])
+     xlabel('WIE')
+     ylabel('WII')
+     set(gca,'FontSize',20)
+     xlim([5.6 8.1])
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=initWIEp;
+    y1=initWIIp;
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2,'color',[0, 0.4470, 0.7410]);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+    saveas(gcf,'preWinit','jpg')
+    saveas(gcf,'preWinit','svg')
+
+    %%
+    
+      WEEpo = sum(WEE,1); %update sum of presynaptic weights for plot
+      WEIpo = sum(WEI,1);
+      WIEpo = sum(WIE,1);
+      WIIpo = sum(WII,1);
+      
+    figure('Position', [10 10 1200 500]);     
+     %suptitle(['Total current per neuron ',titlet])
+     subplot(1,2,1)
+     plot(WEEp,WEEpo,'o','color',[0 0.5 0])
+     xlabel('WEEp')
+     ylabel('WEEpo')
+     set(gca,'FontSize',20)
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=WEEp;
+    y1=WEEpo';
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+
+    subplot(1,2,2)
+     plot(WIEp,WEIpo,'o','color',[1 0 0])
+     xlabel('WIEp')
+     ylabel('WEIpo')
+     set(gca,'FontSize',20)
+    set(findobj(gca,'type','line'),'linew',3)
+    set(gca,'linew',4)
+    set(gca, 'box', 'off')
+    hold on
+    x=WIEp;
+    y1=WEIpo';
+    [R,p] = corr(x,y1,'rows','complete');
+    P = polyfit(x,y1,1);
+    yfit = P(1)*x+P(2);
+    plot(x,yfit,'b','LineWidth',2);
+    title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+    saveas(gcf,'outputW','jpg')
+
+
+end
+
+ 
---
+++ Fig5_crosshomeo_demo_multi_unit_graphics.m
@@ -0,0 +1,128 @@
+
+%called by multi_unit_Up_Dev
+%ssaray@ucla.edu
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PLOTTING %%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Firing rate over time figure (2 E and 2 I example), with 2 example weights. Pulled firing rate of all neurons. 
+if(1)
+     h1=figure('Renderer', 'painters', 'Position', [10 10 1200 1000]);
+     set(gcf,'color','w');
+     hold on
+        subplot(5,2,1)
+        plot(dt-EvokedOn*dt:dt:tmax*dt-EvokedOn*dt,hR(:,18),'color',[0 0.5 0],'linewidth',2,'ydatasource','hR(:,18)'); 
+        line([dt-EvokedOn*dt tmax*dt-EvokedOn*dt],[ExSet ExSet],'color',[0 0.5 0],'linestyle',':','linewidth',2);
+        ylim([0 20])
+        %xlim([dt-EvokedOn*dt tmax*dt-EvokedOn*dt])
+        xlim([dt-EvokedOn*dt 0.7])
+        ylabel('E (Hz)','FontSize',16)
+        set(gca,'FontSize',12)
+
+
+        subplot(5,2,3)
+        plot(dt-EvokedOn*dt:dt:tmax*dt-EvokedOn*dt,hR(:,40),'color',[0 0.5 0],'linewidth',2,'ydatasource','hR(:,40)');   
+        line([dt-EvokedOn*dt tmax*dt-EvokedOn*dt],[ExSet ExSet],'color',[0 0.5 0],'linestyle',':','linewidth',2);
+        ylim([0 20])
+        xlim([dt-EvokedOn*dt 0.7])
+        set(gca,'FontSize',12)
+
+        
+        subplot(5,2,5)
+        plot(dt-EvokedOn*dt:dt:tmax*dt-EvokedOn*dt,hR(:,Ne+1),'color',[1 0 0],'linewidth',2,'ydatasource','hR(:,Ne+1)');  
+        line([dt-EvokedOn*dt tmax*dt-EvokedOn*dt],[InhSet InhSet],'color',[1 0 0],'linestyle',':','linewidth',2);
+        ylim([0 55])
+        xlim([dt-EvokedOn*dt 0.7])
+        ylabel('I (Hz)','FontSize',16)
+        set(gca,'FontSize',12)
+
+
+        subplot(5,2,7)
+        plot(dt-EvokedOn*dt:dt:tmax*dt-EvokedOn*dt,hR(:,Ne+12),'color',[1 0 0],'linewidth',2,'ydatasource','hR(:,Ne+12)');
+        line([dt-EvokedOn*dt tmax*dt-EvokedOn*dt],[InhSet InhSet],'color',[1 0 0],'linestyle',':','linewidth',2);
+        xlabel('Time (sec)','Fontsize',16)
+        ylim([0 55])
+        xlim([dt-EvokedOn*dt 0.7])
+        set(gca,'FontSize',12)
+
+        
+        %subplot(5,2,[9 10])
+        subplot(5,2,9)
+        plot(NaN(nTrial,20),'color',[77/255 166/255 77/255 0.25],'ydatasource','trialhistFCa(:,1:20)','linewidth',1);%[0 0.5 0]
+        hold on
+        plot(NaN(nTrial,1),'color',[77/255 166/255 77/255],'ydatasource','nanmean(trialhistFCa,2)','linewidth',4);%[0 0.5 0]
+        plot(NaN(nTrial,Ni),'color',[255/255,112/255,112/255 0.25],'ydatasource','trialhistFCaInh','linewidth',1); %[255/255,51/255,51/255]
+        plot(NaN(nTrial,1),'color',[255/255,112/255,112/255],'ydatasource','nanmean(trialhistFCaInh,2)','linewidth',4);%[0 0.5 0]
+        line([1 nTrial],[ExSet ExSet],'color',[0 0.5 0],'linestyle','--','linewidth',2);
+        line([1 nTrial],[InhSet InhSet],'color',[255/255,51/255,51/255],'linestyle','--','linewidth',2);
+        %ylim([0 max(ExSet,InhSet)*3])
+        ylabel('Mean E/I (Hz)','Fontsize',16)
+        xlabel('Trials','Fontsize',16)
+        ylim([0 35]) %25
+        set(gca,'FontSize',14)
+
+        
+        
+        subplot(5,2,[2 4])
+     plot(trialhistWEEp,trialhistWEIp,'-o','MarkerSize',3,'LineWidth',0.5,'color',[196/255,193/255,193/255],'xdatasource','trialhistWEEp','ydatasource','trialhistWEIp');
+     hold on
+     plot(WEEp,WEIp,'o','LineWidth',2,'color',[0 0.5 0],'xdatasource','WEEp','ydatasource','WEIp');
+%      xlim([4.8 5.2])
+%      ylim([1 1.15])
+     xlabel('WEE')
+     ylabel('WEI')
+     set(gca,'FontSize',12)
+     %set(findobj(gca,'type','line'),'linew',3)
+     %set(gca,'linew',4)
+     %set(gca, 'box', 'off')
+     hold on
+     x=WEEp;
+     y1=WEIp;
+    [R,p] = corr(x,y1,'rows','complete');
+     P = polyfit(x,y1,1);
+     yfit = P(1)*x+P(2);
+     plot(x,yfit,'b','LineWidth',2,'xdatasource','x','ydatasource','yfit');
+     %title(['R = ',num2str(R),'p = ',num2str(p)],'FontSize',15)
+     %str = ['R = ',num2str(R),'p = ',num2str(p)];
+     %hTitle1 = title(str,'FontSize',15);
+     
+    
+     
+        subplot(5,2,[6 8])
+     plot(trialhistWIEp,trialhistWIIp,'-o','MarkerSize',3,'LineWidth',0.5,'color',[196/255,193/255,193/255],'xdatasource','trialhistWIEp','ydatasource','trialhistWIIp');
+     hold on
+     plot(WIEp,WIIp,'o','color',[1 0 0],'LineWidth',2,'xdatasource','WIEp','ydatasource','WIIp');
+%      xlim([9.8 10.2])
+%      ylim([1.4 1.6])
+     xlabel('WIE')
+     ylabel('WII')
+     set(gca,'FontSize',12)
+     %set(findobj(gca,'type','line'),'linew',3)
+     %set(gca,'linew',4)
+     %set(gca, 'box', 'off')
+     hold on
+     ix=WIEp;
+     iy1=WIIp;
+     [iR,ip] = corr(ix,iy1,'rows','complete');
+     iP = polyfit(ix,iy1,1);
+     iyfit = iP(1)*ix+iP(2);
+     plot(ix,iyfit,'b','LineWidth',2,'xdatasource','ix','ydatasource','iyfit');
+     %title(['R = ',num2str(iR),'p = ',num2str(ip)],'FontSize',15)
+     %str = ['R = ',num2str(iR),'p = ',num2str(ip)];
+     % = title(str,'FontSize',15);
+end
+
+    
+%% MEAN RATE OVER TIME ON IMAGESC  
+
+if(0)
+    figure
+    rImage = imagesc([trialhistFCa';trialhistFCaInh']);
+    colorbar
+    caxis([0 2*InhSet])
+    xlabel('trials')
+    ylabel('units')
+    title('rate over trials')
+end
+
